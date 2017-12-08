455eda26c5ed47d31bee1157c1d7df3daedacd13
CarlosWeber <31441593+CarlosWeber@users.noreply.github.com>
2017-12-08 15:50:40 -0200

    Add files via upload

--- src/model/Aluno.java
+++ src/model/Aluno.java
@@ -1,5 +1,18 @@
 package model;
 
 public class Aluno {
-
+	private String nome;
+	private String email;
+	private int idade;
+	private String cidade;
+	
+	public Aluno (String nome, String email, int idade, String cidade) {
+		this.nome= nome;
+		this.email=email;
+		this.idade=idade;
+		this.cidade=cidade;
+		
+		
+	}
+	
 }
